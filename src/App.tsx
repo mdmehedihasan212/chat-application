3597860f83b0f226b8b7c83b9009f415937d3c6b
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-06-01 15:45:55 +0600

    import multi chat logic and props multi chat window

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { MultiChatWindow } from 'react-chat-engine-advanced';
+import { MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced';
+
+const projectId: string = '90937f25-e701-4efb-b1f7-493e2815cf0f';
+const user: string = 'Mehedi';
+const secret: string = '123456';
 
 function App() {
+  const chatProps = useMultiChatLogic(projectId, user, secret)
   return (
     <div>
-      <MultiChatWindow style={{ height: '100vh' }} />
+      <MultiChatWindow {...chatProps} style={{ height: '100vh' }} />
     </div>
   );
 }
